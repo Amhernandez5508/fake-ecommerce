fdadf1a50320cc10c20d51abda89a60b09d1cc8b
amhernandez5508 <ahernandez5508@gmail.com>
2024-02-24 16:22:59 -0300

    Initial archives upload

---
+++ js/adminScript.js
@@ -0,0 +1,16 @@
+const table = document.querySelector("#userTable");
+const url = "http://localhost:8080/api/v1/users";
+
+const xhr = new XMLHttpRequest();
+xhr.open("GET", url + "/");
+xhr.responseType = "json";
+xhr.send();
+
+xhr.onload = function() {
+    xhr.response.forEach(el => {
+        table.children.item(1).appendChild(document.createElement("tr"));
+        table.children.item(1).lastChild.innerHTML =  ` <td>${el.id}</td>
+                                                        <td>${el.username}</td>
+                                                        <td>${el.role}</td>`;
+    });
+}
